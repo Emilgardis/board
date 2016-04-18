b54b976517f645ea1966330406073e301c4ff790
Emil Gardström <emil.gardstrom@gmail.com>
2016-04-18 13:53:50 +0200

    Added more info on `.lib` files.
    Some fixes also.

--- src/lib/file_reader.rs
+++ src/lib/file_reader.rs
@@ -9,19 +9,66 @@ use board_logic::{Board, BoardMarker, Stone, Point};
 
 /// Describes the file
 pub enum FileType{
-    /// `.pos` file.
+    /// Generic Renju _.pos_ file.
     ///
-    /// `.pos` files seems to always assume a field of size 15*15
-    /// # Layout
-    /// 0: N moves
-    /// 1: Black move #1
-    /// 2: White move #2
-    /// N: Last move
+    /// These files seems to always assume a field of size 15*15
+    /// # Layout in binary
+    /// * 0: N moves
+    /// * 1: Black move #1
+    /// * 2: White move #2
+    /// * N: Last move
     /// 
     Pos,
-    /// `.lib` file.
+    /// RenLib, _.lib_ file.
     ///
-    /// This is not supported yet as I have not reversed the protocol yet.
+    /// This is not supported yet as I have not fully reversed the protocol yet.
+    /// 
+    /// # About
+    ///
+    /// _.lib_ files are most used for it's excellent support for saving multiple games of one play. This enables 
+    /// a way to do analysis. 
+    ///
+    /// #### Example
+    /// The move sequence is H8 I9 G7 H7 D11 etc. We want to analyse and document what would
+    /// have happened if we played H6 instead of H7 on move #4. So we would go back to move #3 and continue from there. Now,
+    /// when walking through the game from the beginning, we will see that there are 2 choices 
+    /// highlighted on move #4, we have created a _sub game_.
+    /// Clicking on either will continue this pattern of finding sub-games like before, repeating itself if needed. If
+    /// you don't choose any of those choices it makes a new sub-game.
+    /// 
+    /// This is why RenLib's _.lib_ format is so praised in the renju community.
+    ///
+    /// ...* _.lib_ files come in two variations, either as Libraries or as Positions. I have yet
+    /// to identify what the signature is for both.
+    ///
+    /// ## Known:
+    /// * Libraries seem to be saved with the signature 0x40 ("@") some where at the end.
+    /// Positions seems to be stored as two bytes. With 0x78 being the middle.
+    /// With what I know so far, after 10 "0xFF", the first move is stored. Then if the next byte is "0x00",
+    /// continue and repeat. How sub-games are stored will soon be cracked.
+    ///     
+    ///     This is the layout:
+    ///     
+    ///          0: . . . . . . . . . . . . . . .  
+    ///          1: . . . . . . . . . . . . . . . 
+    ///          2: . . . . . . . . . . . . . . . 
+    ///          3: . . . . . . . . . . . . . . . 
+    ///          4: . . . O . . . . . . . . . . . 
+    ///          5: . . . . . . . . . . . . . . . 
+    ///          6: . . . . . . . . . . . . . . . 
+    ///          7: . . . . . . . X . . . . . . . 
+    ///          8: . . . . . . . . . . . . . . . 
+    ///          9: . . . . . . . . . . . . . . . 
+    ///          A: . . . . . . . . . . . . . . . 
+    ///          B: . . . . . . . . . . . . . . . 
+    ///          C: . . . . . . . . . . . . . . . 
+    ///          D: . . . . . . . . . . . . . . . 
+    ///          E: . . . . . . . . . . . . . . . 
+    ///             1 2 3 4 5 6 7 8 9 A B C D E F  
+    ///
+    /// The _O_ is on `0x44`, the _X_ is on `0x78` (the middle)
+    /// 
+    /// *    Libraries usually have "@" at the end, this
     Lib,
 }
 
