5cc8dffd352663a6da40f1104989254b6b40c78b
Emil Gardström <emil.gardstrom@gmail.com>
2016-04-21 11:16:50 +0200

    Fixing this some more, most stuff should work, still working on the
    borrow state.

--- src/lib/move_node.rs
+++ src/lib/move_node.rs
@@ -25,42 +25,42 @@ pub fn ref_mut_filter_map<
 
 #[derive(Debug)]
 struct Node<'a> {
-    children: RefCell<Vec<Rc<Node<'a>>>>, // Should it be RefCell instead of Rc?
+    children: Vec<RefCell<Rc<Node<'a>>>>, // Should it be RefCell instead of Rc?
     parent: Option<Weak<Node<'a>>>, // Weak instead of Rc.
-    mark: RefCell<Option<BoardMarker>>, // Should this be a Cell? Or should it own the BoardMarker?
+    mark: Option<BoardMarker>, // Should this be a Cell? Or should it own the BoardMarker?
 }
 
 impl<'a> Node<'a> {
-    fn new_root() -> Rc<Node<'a>> {
-        Rc::new(Node {
-            children: RefCell::new(Vec::new()),
+    fn new_root() -> RefCell<Rc<Node<'a>>> {
+        RefCell::new(Rc::new(Node {
+            children: Vec::new(),
             parent: None,
-            mark: RefCell::new(None),
-        }) }
+            mark: None,
+        })) }
     fn new(mark: BoardMarker, parent: Weak<Node<'a>>) -> Node<'a> {
         Node {
-            children: RefCell::new(Vec::new()),
+            children: Vec::new(),
             parent: Some(parent),
-            mark: RefCell::new(Some(mark)),
+            mark: Some(mark),
         }
     }
     // fn find_child(mark: Pos) -> Result<Rc<Node>, bool> ?
-    fn add_child(&self, mark: BoardMarker, root: Rc<Node<'a>>) -> Result<Ref<Rc<Node<'a>>>, bool> {
-
-        self.children.borrow_mut().push(Rc::new(Node::new(mark, Rc::downgrade(&root))));
-        Ok(self.get_nth(self.children.borrow().len()-1).unwrap())
+    fn add_child(&mut self, mark: BoardMarker, root: RefCell<Rc<Node<'a>>>) -> Result<RefCell<Rc<Node<'a>>>, bool> {
+        {
+            self.children.push(RefCell::new(Rc::new(Node::new(mark, Rc::downgrade(&root.borrow())))));
+        }
+        Ok(self.get_nth(&self.children.len()-1).unwrap())
     }
     
     fn is_root(&self) -> bool {
         self.parent.is_none()
     }
     
-    fn get_nth(&self, index: usize) -> Result<Ref<Rc<Node<'a>>>, bool>{
-        let temp: Ref<Vec<Rc<Node>>> = self.children.borrow(); // children: RefCell<Vec<Rc<Node<'a>>>>
-        match ref_filter_map(temp, |children| children.get(index)) {
-            Some(val) => Ok(val),
+    fn get_nth(&self, index: usize) -> Result<RefCell<Rc<Node<'a>>>, bool>{
+        match self.children.get(index) {
+            Some(val) => Ok(val.clone()),
             None => Err(true),
-        }
+        } // children: Vec<RefCell<Rc<Node<'a>>>>
     }
     
     //fn get_parent(&self) -> Result<Weak<Node<'a>>, bool>{
@@ -71,13 +71,15 @@ impl<'a> Node<'a> {
 
 #[test]
 fn check_if_node_kinda_works() {
-    let mut root: Rc<Node> = Node::new_root();
-    println!("Is root: {}", root.is_root());
-    let child1: Ref<Rc<Node>> = root.add_child(BoardMarker { point: Point { x: 0, y: 0}, color: Stone::Black}, root.clone()).unwrap();
+    let mut root: RefCell<Rc<Node>> = Node::new_root();
+    println!("Is root: {}", root.borrow().is_root());
+    let mut child1: RefCell<Rc<Node>> = root.borrow_mut().add_child(BoardMarker { point: Point { x: 0, y: 0}, color: Stone::Black}, root.clone()).unwrap();
     // let child1: Ref<Rc<Node>> =  root.get_nth(0).unwrap();
-    assert_eq!(format!("{:?}", child1), format!("{:?}", root.get_nth(0).unwrap()));
-    {let mut child1mark: RefMut<Option<BoardMarker>> = child1.mark.borrow_mut();
-    *child1mark = Some(BoardMarker { point: Point { x: 14, y: 14}, color: Stone::Black});}
-    println!("{:?}", root.get_nth(0));
+    assert_eq!(format!("{:?}", child1), format!("{:?}", root.borrow().get_nth(0).unwrap()));
+    {
+        let mut marker: RefMut<Rc<Node>> = child1.borrow_mut();
+        marker.mark = Some(BoardMarker { point: Point { x: 14, y: 14}, color: Stone::Black});
+    }
+    println!("{:?}", root.borrow().get_nth(0));
     //println!("{:?}", root.get_parent());
 }
